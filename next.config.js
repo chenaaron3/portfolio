ff0e5b7d2c8978b244f003f77b85e3cb6477b9d7
Aaron Chen <chenaaron3@gmail.com>
2024-03-11 23:39:55 -0700

    Remove setup pages action

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  images: {unoptimized: true},
+  basePath: "/portfolio",
   output: "export",
   reactStrictMode: true,
 
